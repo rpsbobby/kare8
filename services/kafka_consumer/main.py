b5d1730cde38ef0f78b74018fcb19dced3819b9d
robert-szlufik <robert.szlufik123@gmail.com>
2025-08-16 12:26:47 +0100

    fixing missing arg

--- services/kafka_consumer/main.py
+++ services/kafka_consumer/main.py
@@ -22,8 +22,8 @@ if __name__ == "__main__":
     kafka_consumer = get_kafka_consumer()
     kafka_producer = get_kafka_producer()
     worker = OrderWorker(logger=logger)
-    message_handler = MessageHandler(kafka_producer=kafka_producer, worker=worker, model_cls=Order, topic_in=TOPIC_IN, dlq_topic=DLQ_TOPIC,
-                                     park_topic=PARK_TOPIC, logger=logger, max_attempts=MAX_ATTEMPTS)
+    message_handler = MessageHandler(kafka_producer=kafka_producer, worker=worker, model_cls=Order, topic_in=TOPIC_IN, topic_out=TOPIC_OUT,
+                                     dlq_topic=DLQ_TOPIC, park_topic=PARK_TOPIC, logger=logger, max_attempts=MAX_ATTEMPTS)
 
     logger.info(f"Connected to Kafka. Subscribing to '{TOPIC_IN}'...")
 
