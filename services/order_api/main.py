0dafaeef206c87ef02300a11b6f96fafb6cb928f
robert-szlufik <robert.szlufik123@gmail.com>
2025-08-17 18:24:39 +0100

    updating order_api to latest consumer + wokrer

--- services/order_api/main.py
+++ services/order_api/main.py
@@ -1,18 +1,43 @@
+import os
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 from entities.order import Order
+from handlers.message_handler import MessageHandler
 from messaging.factories.kafka_factory import get_kafka_producer
+from messaging.workers.order_worker import OrderWorker
 from utils.logger import get_logger
-from topics.topics import ORDERS
-
-app = FastAPI()
-
-logger = get_logger("order_api")
-
-kafka_producer = get_kafka_producer();
+from topics.topics import ORDERS, ORDERS_DLQ, ORDERS_PARK, GENERATE_INVOICE
+from o11y.metrics import start_metrics_server
 
 
+logger=get_logger("order_api")
+PROMETHEUS_SERVER= int(os.getenv("PROMETHEUS_SERVER", "9000"))  # Port for Prometheus metrics
+TOPIC_IN = os.getenv("TOPIC", "NONE")
+DLQ_TOPIC = os.getenv("DLQ_TOPIC", ORDERS_DLQ)
+PARK_TOPIC = os.getenv("PARK_TOPIC", ORDERS_PARK)
+TOPIC_OUT = os.getenv("TOPIC_OUT", ORDERS)
+MAX_ATTEMPTS = int(os.getenv("MAX_ATTEMPTS", "3"))
+
+kafka_producer=get_kafka_producer()
+worker=OrderWorker(logger=logger)
+message_handler=MessageHandler(kafka_producer=kafka_producer, worker=worker, model_in=Order, topic_in=TOPIC_IN, topic_out=TOPIC_OUT,
+                               dlq_topic=DLQ_TOPIC, park_topic=PARK_TOPIC, logger=logger, max_attempts=MAX_ATTEMPTS)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    start_metrics_server(PROMETHEUS_SERVER)  # separate port for Prometheus scraping
+    logger.info(f"✅ Metrics server started on :{PROMETHEUS_SERVER}")
+
+    yield  # 👈 FastAPI will run the app while we are in this context
+
+    # Shutdown (if needed)
+    logger.info("🛑 Shutting down order_api service")
+
+app=FastAPI(lifespan=lifespan)
+#
 @app.post("/order")
 def create_order(order: Order):
     logger.info(f"Received order request {order.order_id}")
---
+++ shared/o11y/metrics.py
@@ -0,0 +1,26 @@
+# metrics.py
+from prometheus_client import Counter, Histogram, Gauge, start_http_server
+
+# Counter: goes up only
+messages_processed_total = Counter(
+    "messages_processed_total",
+    "Total number of processed messages",
+    ["topic", "status"]  # labels
+)
+
+# Histogram: latency buckets
+processing_latency_seconds = Histogram(
+    "processing_latency_seconds",
+    "Message processing latency in seconds",
+    ["topic"]
+)
+
+# Gauge: point-in-time values
+queue_depth = Gauge(
+    "queue_depth",
+    "Current producer queue size",
+    ["topic"]
+)
+
+def start_metrics_server(port: int = 8000):
+    start_http_server(port)
