2afe167348f85d5b613816b6e762e166845d60f0
robert-szlufik <robert.szlufik123@gmail.com>
2025-08-20 20:01:14 +0100

    fixes kafka factory in api_service

--- services/order_api/messaging/factories/kafka_factory.py
+++ services/order_api/messaging/factories/kafka_factory.py
@@ -1,7 +1,9 @@
+import os
+
 from messaging_interfaces.kafka.kafka_producer_interface import KafkaProducerInterface
 from ..impl.kafka_producer import KafkaProducer
 
 
 def get_kafka_producer() -> KafkaProducerInterface:
-    # bootstrap_servers = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9092")
-    return KafkaProducer("kafka:9092")
+    kafka_host = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9092")
+    return KafkaProducer(kafka_host)
